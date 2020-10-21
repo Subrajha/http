084aea79d10ba0fe29d1249e101f7bcbb60a418f
Subrajha <subrajha52@gmail.com>
2020-10-22 01:11:08 +0530

    169,170 showing ui body and handling request errors

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -4,43 +4,41 @@ import './FullPost.css';
 import axios from 'axios';
 
 class FullPost extends Component {
-    state={
-        selectedposts:null
+    state = {
+        selectedposts: null
     }
-    componentDidUpdate(){
-        if((!this.state.selectedposts && this.props.id)||(this.props.id && this.state.selectedposts&& this.state.selectedposts.id!==this.props.id))
-        {
-            axios.get('https://jsonplaceholder.typicode.com/posts/'+this.props.id).then(response=>{
-                this.setState({selectedposts:response.data});
+    componentDidUpdate() {
+        if ((!this.state.selectedposts && this.props.id) || (this.props.id && this.state.selectedposts && this.state.selectedposts.id !== this.props.id)) {
+            axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id).then(response => {
+                this.setState({ selectedposts: response.data });
             });
         }
-       
+
     }
-    
-deletePost=()=>{
-    axios.delete('https://jsonplaceholder.typicode.com/posts/'+this.props.id).then(response=>{
-        console.log(response);
-    });
-}
-    render () {
+
+    deletePost = () => {
+        axios.delete('https://jsonplaceholder.typicode.com/posts/' + this.props.id).then(response => {
+            console.log(response);
+        });
+    }
+    render() {
         let post = <p className="FullPost">Please select a Post!</p>;
-        if(this.props.id){ 
-        post=<p className="FullPost">Please select a Post!</p>;
-    }
-        if( this.state.selectedposts)
-        {
+        if (this.props.id) {
+            post = <p className="FullPost">Please select a Post!</p>;
+        }
+        if (this.state.selectedposts) {
             post = (
                 <div className="FullPost">
                     <h1>{this.state.selectedposts.title}</h1>
-                    <p>Content</p>
+                    <p>{this.state.selectedposts.body}</p>
                     <div className="Edit">
                         <button className="Delete" onClick={this.deletePost}>Delete</button>
                     </div>
                 </div>
-    
+
             );
         }
-       
+
         return post;
     }
 }
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,40 +7,50 @@ import './Blog.css';
 import axios from 'axios';
 
 class Blog extends Component {
-    state={
-        posts:[],
-        selectedId:null
+    state = {
+        posts: [],
+        selectedId: null,
+        error: null
     }
-    componentDidMount(){
-        axios.get('https://jsonplaceholder.typicode.com/posts').then((response)=>{
-            const posts=response.data.splice(0,4);
-            const updatedposts=posts.map((data)=>{
-                return {
-                    ...data,
-                    author:"max"
-                }
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then((response) => {
+                const posts = response.data.splice(0, 4);
+                const updatedposts = posts.map((data) => {
+                    return {
+                        ...data,
+                        author: "max"
+                    }
+                })
+                this.setState({ posts: updatedposts });
             })
-            this.setState({posts:updatedposts})
-        });
+            .catch(error => {
+                this.setState({ error: error });
+            });
     }
-    selectedPostID(id){
-        this.setState({selectedId:id});
+    selectedPostID(id) {
+        this.setState({ selectedId: id });
     }
-    render () {
-        const posts=this.state.posts.map((res)=>{
-            return <Post 
-            key={res.id} 
-            title={res.title} 
-            author={res.author}
-            clicked={()=>this.selectedPostID(res.id)}/>
-        })
+    render() {
+        let posts;
+        if (this.state.error) posts = <p>something wrong</p>
+        else {
+            posts = this.state.posts.map((res) => {
+                return <Post
+                    key={res.id}
+                    title={res.title}
+                    author={res.author}
+                    clicked={() => this.selectedPostID(res.id)} />
+            })
+        }
+
         return (
             <div>
                 <section className="Posts">
                     {posts}
                 </section>
                 <section>
-                    <FullPost id={this.state.selectedId}/>
+                    <FullPost id={this.state.selectedId} />
                 </section>
                 <section>
                     <NewPost />
